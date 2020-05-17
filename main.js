a689bfa6b101cdbbad1382d0ccee8a7c694c8e0c
Kamila Zubala <k.zubala@gmail.com>
2020-05-17 12:55:10 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const firstName= 'Kamila';
+const age= 33;
+
+console.log (firstName);
+console.log (age);
+console.log (`Hej, jestem właścicielką kaktusiarniokawiarni Kaktus na dłoni. Mam na imię ${firstName} i mam ${age} lata.`)
