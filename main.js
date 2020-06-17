b01a7258f632360ee56ed7e7479375c44df1b46a
Kamila Zubala <k.zubala@gmail.com>
2020-06-17 17:29:56 +0100

    troche fakap

--- main.js
+++ main.js
@@ -15,3 +15,57 @@ function createContent(querySelectorContent, content) {
 }
 
 createContent(".main-page--js", "Odpowiemy na każdą wiadomość!");
+
+const humanOne = {
+  name: 'Kamila',
+  age: 33,
+  address: {
+    city: 'London',
+    country: 'UK'
+  }
+}
+
+const humanTwo = {
+  name: 'Zuzanna',
+  age: 31,
+  address: {
+    city: 'Lublin',
+    country: 'Poland'
+  }
+}
+
+if (humanOne.age > humanTwo.age) {
+  console.log('Kamila jest starsza');
+}
+  else if (humanOne.age < humanTwo.age) {
+  console.log('Zuzanna jest starsza');
+  } 
+else if (humanOne.age === humanTwo.age) {
+  console.log('Są w tym samym wieku');
+}
+
+const myNumber = {name: 'Kamila'};
+
+switch (myNumber) {
+  case 5:
+    console.log ('Jestem piątką');
+    break;
+    case 7:
+      console.log ('Jestem siódemką');
+      break;
+      default:
+      console.log ('Jestem czymś innym');
+}
+
+(100 > 50) ? console.log('To prawda') : console.log('To nieprawda');
+
+const result = (100 > 50) ? {name: 'Kamila'} : false;
+
+console.log(result);
+
+const hamburger = document.querySelector('.hamburger--js');
+
+hamburger.addEventListener('click', () => {
+  const nav = document.querySelector('.navigation--js');
+  nav.classList.toggle('navigation--open');
+})
